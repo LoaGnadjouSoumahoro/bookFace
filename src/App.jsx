7f66a40f680ab36962dc7cf17db3cfc80a97f9c7
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-02-20 15:49:42 +0100

    le layout de la page

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,18 @@
-import { useState } from "react";
-import styled from "@emotion/styled";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
-import "./App.css";
-import { Button, Typography } from "@mui/material";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import { Box, Stack, Container } from "@mui/material";
 
 function App() {
-  const BlueButton = styled(Button)({
-    backgroundColor: "skyblue",
-    color: "#888",
-    margin: 5,
-    "&:hover": {
-      backgroundColor: "lightblue",
-    },
-    "&:disabled": {
-      backgroundColor: "gray",
-      color: "white",
-    },
-  });
   return (
-    <div>
-      <Button variant="text">Text</Button>
-      <Button
-        startIcon={<SettingsOutlinedIcon />}
-        variant="contained"
-        color="secondary"
-      >
-        Settings
-      </Button>
-      <Button
-        startIcon={<AddOutlinedIcon />}
-        variant="contained"
-        color="success"
-      >
-        Add new Post
-      </Button>
-      <Button variant="outlined">Outlined</Button>
-      <Typography variant="h1" component="p">
-        It uses h1 style but
-      </Typography>
-      <BlueButton> My Button</BlueButton>
-      <BlueButton> Another Button</BlueButton>
-    </div>
+    <Box>
+      <Stack direction="row" spacing={2} justifyContent={"space-between"}>
+        {/* Navbar */}
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
