a4adcf62468b5168af96d924ea6198d0b4f7ba36
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-02-20 17:14:04 +0100

    Ajout navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
 import { Box, Stack, Container } from "@mui/material";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Box>
+      <Navbar />
       <Stack direction="row" spacing={2} justifyContent={"space-between"}>
-        {/* Navbar */}
         <Sidebar />
         <Feed />
         <Rightbar />
