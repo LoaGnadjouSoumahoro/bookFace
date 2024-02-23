5b4b7c5ad786a34d6e83c1a3d4d4ca5c9dfaf077
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-02-23 11:29:15 +0100

    résolution de conflit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
-import { Box, Stack, Container } from "@mui/material";
+import {
+  Box,
+  Stack,
+  Container,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
 import Navbar from "./components/Navbar";
 import Add from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <Box>
-      <Navbar />
-      <Stack direction="row" spacing={2} justifyContent={"space-between"}>
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
-      <Add />
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"Background.default"} color={Text.primary}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent={"space-between"}>
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
